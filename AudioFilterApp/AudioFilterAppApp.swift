1dbc99d7eab073137c3396f8c96fc6944536df0b
Sviatoslav Ivanov <sviatoslav@arma-app.com>
2023-02-02 18:00:23 +0100

    Initial Commit

---
+++ AudioFilterApp/AudioFilterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioFilterAppApp.swift
+//  AudioFilterApp
+//
+//  Created by Sviatoslav Ivanov on 2/2/23.
+//
+
+import SwiftUI
+
+@main
+struct AudioFilterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
